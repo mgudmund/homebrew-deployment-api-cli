9ea1aa6fe71b21b5cb37768d71d87f4db644997f
margud <martin.gudmundsson@ikea.com>
2020-05-25 10:14:22 +0200

    fixed classname

--- formula/deployment-api-cli.rb
+++ formula/deployment-api-cli.rb
@@ -4,7 +4,7 @@ class DeploymentApiCli < Formula
   desc "CLI for deployment metrics api"
   homepage "https://github.com/mgudmund/deployment-api-cli"
   url "https://github.com/mgudmund/deployment-api-cli/releases/download/v1.0-alpha/deployment-api-cli-v1.0-alpha.macosx.tar.gz"
-  sha256 "i5e46ad198823e2fa77879fdeef043701cc40c4d85ef2c77aea4225f47ddfddf5"
+  sha256 "5e46ad198823e2fa77879fdeef043701cc40c4d85ef2c77aea4225f47ddfddf5"
   head "https://github.com/mgudmund/deployment-api-cli"
 
   def install
