08910b3e4ada1a1a73f4dc02e020a2cd7845e248
margud <martin.gudmundsson@ikea.com>
2020-05-25 12:25:02 +0200

    Added Linux support

--- formula/deployment-api-cli.rb
+++ formula/deployment-api-cli.rb
@@ -3,10 +3,15 @@ require "formula"
 class DeploymentApiCli < Formula
   desc "CLI for deployment metrics api"
   homepage "https://github.com/mgudmund/deployment-api-cli"
-  url "https://github.com/mgudmund/deployment-api-cli/releases/download/v1.0-alpha/deployment-api-cli-v1.0-alpha.macosx.tar.gz"
-  sha256 "be23db7d812ff00a0896c5a791d0b476ad00910dcc5772fbc4924215621f5ba1"
+  if OS.mac?
+    url "https://github.com/mgudmund/deployment-api-cli/releases/download/v1.0-alpha/deployment-api-cli-v1.0-alpha.macosx.tar.gz"
+    sha256 "be23db7d812ff00a0896c5a791d0b476ad00910dcc5772fbc4924215621f5ba1"
+  elsif OS.linux?
+    url "https://github.com/mgudmund/deployment-api-cli/releases/download/v1.0-alpha/deployment-api-cli-v1.0-alpha.x86_64-linux.tar.gz"
+    sha256 "a2439e32012c203e8a750f66d297e63f85b2612ff28617fb7879280448d30861"
+  end
   head "https://github.com/mgudmund/deployment-api-cli"
-
+  
   def install
     bin.install "deployment-api-cli"
   end
