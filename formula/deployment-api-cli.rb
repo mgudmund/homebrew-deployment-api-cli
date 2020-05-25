8415de86cd8c56410a2ae4e583a03d1e02dafa06
margud <martin.gudmundsson@ikea.com>
2020-05-25 13:02:17 +0200

    fixed linux install problem

--- formula/deployment-api-cli.rb
+++ formula/deployment-api-cli.rb
@@ -7,7 +7,7 @@ class DeploymentApiCli < Formula
   if OS.mac?
     url "https://github.com/mgudmund/deployment-api-cli/releases/download/v1.0-alpha/deployment-api-cli-v1.0-alpha.macosx.tar.gz"
     sha256 "be23db7d812ff00a0896c5a791d0b476ad00910dcc5772fbc4924215621f5ba1"
-  elsif OS.linux?
+  else
     url "https://github.com/mgudmund/deployment-api-cli/releases/download/v1.0-alpha/deployment-api-cli-v1.0-alpha.x86_64-linux.tar.gz"
     sha256 "a2439e32012c203e8a750f66d297e63f85b2612ff28617fb7879280448d30861"
   end
